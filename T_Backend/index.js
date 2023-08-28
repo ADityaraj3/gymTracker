ae549bf45bf35ebee6ef8d3bd326303e462329e1
Aditya <adityasraj123@gmail.com>
2023-08-28 12:04:38 +0530

    Backend data dispalyed

--- T_Backend/index.js
+++ T_Backend/index.js
@@ -2,12 +2,29 @@ import express from 'express'
 import mongoose from 'mongoose'
 import Obj from './models/object.js'
 import { objs } from './data/index.js'
-
-const port = 3000
+import helment from 'helmet';
+import cors from 'cors';
+const port = 5173
 const mongoURL = 'mongodb+srv://khemesarashreyansh:wSvCBS6ZjupLRLqz@gymtracker.6b7y63a.mongodb.net/?retryWrites=true&w=majority';
 const dbName = 'gymTracker';
 
-const app = express()
+const app = express();
+app.use(express.json());
+app.use(helment());
+app.use(helment.crossOriginResourcePolicy({ policy: "cross-origin" }));
+const allowedOrigins = ['http://localhost:3002'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 
 mongoose.connect(mongoURL, {
     dbName: dbName,
@@ -20,8 +37,8 @@ mongoose.connect(mongoURL, {
 
 app.get('/getUserData', async (req, res)=>{
     try {
-        const data = await Obj.find()
-        res.status(200).json(data)
+        const data = await Obj.find();
+        res.status(200).json(data);
     } catch (err ) {
         console.log(err)
         res.status(404).json({"msg" : err })
--- t_frontend/src/App.tsx
+++ t_frontend/src/App.tsx
@@ -1,22 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState,useEffect } from 'react';
 import './App.css';
 import Card from './Card'
 
 
 function App() {
-  const exercise = "Push-up"; // Replace with your exercise name
-  const weight = 50; // Replace with your weight value
-  const type = "Strength"; // Replace with your type
-  const reps = 10; // Replace with your reps value
+  // const exercise = "Push-up"; // Replace with your exercise name
+  // const weight = 50; // Replace with your weight value
+  // const type = "Strength"; // Replace with your type
+  // const reps = 10; // Replace with your reps value
+
+  const [datas,setDatas] = useState([]);
+  const getData = async () => {
+    const response = await fetch("http://localhost:5173/getUserData", {
+      method: "GET",
+    });
+    const data = await response.json();
+    setDatas(data);
+    }
+
+    useEffect(() => {
+      getData();
+    }, []); 
 
   return (
     <div className="App">
-      <Card
-      exercise={exercise}
-      weight={weight}
-      type={type}
-      reps={reps}/>
+      {datas.map(({
+        _id,
+        exercise,
+        weight,
+        type,
+        reps,
+      })=>(
+        <Card
+        key={_id}
+        exercise={exercise}
+        weight={weight}
+        type={type}
+        reps={reps}
+        />
+      )
+      )}
     </div>
   );
 }
